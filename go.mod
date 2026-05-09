674179856e59f3b78cdb04374edf919ce172db94
Mykola Volkoboi <nik.volkoboy@gmail.com>
2026-05-09 17:44:52 +0300

    mySQL connection added

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module lab5-server
 
 go 1.26.3
 
-require github.com/go-chi/chi/v5 v5.2.5 // indirect
+require (
+	filippo.io/edwards25519 v1.2.0 // indirect
+	github.com/go-chi/chi/v5 v5.2.5 // indirect
+	github.com/go-sql-driver/mysql v1.10.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,17 +1,33 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/pets") //user:password@/dbname
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	fmt.Println("Database connected")
 
 	r := chi.NewRouter()
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		res := []byte("Hello, World!")
-		w.Write(res)
+		var greeting string
+		err := db.QueryRow("SELECT 'Hello, World!'").Scan(&greeting)
+		if err != nil {
+			http.Error(w, "Database query failed", http.StatusInternalServerError)
+			return
+		}
+		w.Write([]byte(greeting))
 	})
 	http.ListenAndServe(":8080", r)
 }
